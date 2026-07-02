fd28e13a22b7df4d8e39e54df9ec8d1cb4d1c4f6
Jr. <leofredo.junior@gmail.com>
2026-07-01 22:46:45 -0300

    Atualizar o Calculadora II.c

--- Calculadora II.c
+++ Calculadora II.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
-int main () {
-	float x=0, y=0, resultado=0;
-	int op=0;
-	do{
-		printf("\n\t1 - soma\n\t2 - subtracao\n\t3 - multiplicacao\n\t4 - divisao\n");
-		scanf("%i", &op);
-		printf("\nDigite o primeiro numero: ");
-		scanf("%f", &x);
-		printf("\nDigite o segundo numero: ");
-		scanf("%f", &y);
-		
-			switch(op){
-				case 1:
-					resultado = x+y;
-					break;
-				
-				case 2:
-					resultado = x-y;
-					break;
-					
-				case 3:
-					resultado = x*y;
-					break;
-					
-				case 4:
-					resultado = x/y;
-					break;	
-					
-				default:
-					printf("\nDigite uma opcao valida\n");
-						break;
-								
-			}
-			printf("\n\t O resultado e: %0.2f", resultado);
-			printf("\nDigite 1 para continuar: ");
-			scanf("%i", &op);
-			
-	}while(op==1);
+
+int main() {
+
+    float x = 0, y = 0, resultado = 0;
+    int op, continuar;
+
+    do {
+
+        printf("\n\t1 - Soma");
+        printf("\n\t2 - Subtracao");
+        printf("\n\t3 - Multiplicacao");
+        printf("\n\t4 - Divisao");
+        printf("\nEscolha uma opcao: ");
+        scanf("%d", &op);
+
+        if(op < 1 || op > 4){
+            printf("\nOpcao invalida!\n");
+            continue;
+        }
+
+        printf("\nDigite o primeiro numero: ");
+        scanf("%f", &x);
+
+        printf("Digite o segundo numero: ");
+        scanf("%f", &y);
+
+        switch(op){
+
+            case 1:
+                resultado = x + y;
+                break;
+
+            case 2:
+                resultado = x - y;
+                break;
+
+            case 3:
+                resultado = x * y;
+                break;
+
+            case 4:
+                if(y != 0)
+                    resultado = x / y;
+                else{
+                    printf("\nErro: divisao por zero!\n");
+                    continue;
+                }
+                break;
+        }
+
+        printf("\nResultado = %.2f\n", resultado);
+
+        printf("\nDigite 1 para continuar ou qualquer outro numero para sair: ");
+        scanf("%d", &continuar);
+
+    } while(continuar == 1);
+
+    return 0;
 }
